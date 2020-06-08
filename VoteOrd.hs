65f3d0cbbb1ca78f8e198b4f2097ad2fa2919824
baizoukou <baizoukoualexandre@email.fr>
2020-06-08 17:00:34 +0100

    Working on Counting Vote

---
+++ VoteOrd.hs
@@ -0,0 +1,8 @@
+------------- Alexandre Baizoukou ----------
+import Data.List
+import TupFunc
+import DATA
+
+-------The quota is calculated quota = (number of valid votes number of seats + 1 ) + 1 3. ----
+quota :: Int
+quata = 
