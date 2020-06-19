b1042deb67abf138e5e98970ec3346a52d7ea405
baizoukou <baizoukoualexandre@email.fr>
2020-06-19 12:00:15 +0100

    Changed tupfunc to generic and review code line

---
+++ Generic.hs
@@ -0,0 +1,26 @@
+module Generic where
+
+---- Helper generic functions
+
+import Data.Char (isDigit)
+import Data.List (sortBy, maximumBy)
+
+
+type Weight = Float
+
+data Vote = Vote  Weight Int [Int]
+  deriving (Show, Eq, Ord) 
+
+
+-- shorthand for filtering out  an element of a list
+fout :: Eq a => a -> [a] -> [a]
+fout d = filter (/=(d))
+
+-- shorthand for mapping through a matrix of things
+mapMat :: (a -> b) -> [[a]] -> [[b]]
+mapMat f = map (map f)
+
+-- Get a candidates name from a candidate tuple
+getName :: [String] -> (Int, Weight) -> (String, Weight)
+getName cs (ind, wt) = let name = cs !! (ind-1)
+                       in (name, wt)
