e595f57b8df98adcf049917883a6d367a7aaa532
baizoukou <baizoukoualexandre@email.fr>
2020-06-22 21:56:12 +0100

    transforming votes + transforming list of votes

--- Adjust.hs
+++ Adjust.hs
@@ -1,11 +1,18 @@
-
-
-
-
-
-
+module Adjust (
+                commaSep,
+                filterVoters,
+                toStandard,
+                perm,
+                sortByHead,
+                removeSpoiled,
+                accom
+               ) where
 
 import Data.List.Split (splitWhen)
 import Data.List (sortBy, groupBy)
 import Data.Char (isDigit)
 import Generic
+
+-- Break lines in comma sepparated vals
+commaSep :: String -> [String]
+commaSep = splitWhen (==',')
--- Votes.hs
+++ Votes.hs
@@ -5,3 +5,14 @@ module Votes where
 
 import Generic
 import Data.List (sortBy, maximumBy)
+
+-- Transform Integer list to formal Vote
+toVote :: [Int] -> Vote
+toVote (x:xs) = Vote 1.0 x xs
+
+-- Total the weights in a list of votes
+-- If a single candidate, it calculates its current score
+candWeight :: [Vote] -> (Int, Float)
+candWeight []                  = error "CORRECT"
+candWeight l@(Vote _ f _ : xs) = let foo n (Vote w p _) = n + w
+                                  in (f, foldl foo 0 l)
