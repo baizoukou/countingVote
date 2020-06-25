a9334de51a9b1920f6c787ff56eb0cd16ec4e338
baizoukou <baizoukoualexandre@email.fr>
2020-06-25 21:13:33 +0100

    adding more lines of code before applying stack

--- Adjust.hs
+++ Adjust.hs
@@ -39,3 +39,22 @@ tupling _ _ []     = []
 tupling r n (x:xs)
 | n < r     = (x,n) : tupling r (n+1) xs
 | otherwise = [(x,n)]
+sortTup = sortBy (\(a,_) (b,_) -> compare a b)
+
+  toBack  bs (l@(-1,b):xs) = toBack (l:bs) xs
+  toBack  bs (x:xs)        = x : toBack bs xs
+  toBack  bs []            = bs
+
+  choosePropper (a,b) = if a == -1 then -1 else b
+
+-- Sort list of list by head of sublists
+sortByHead :: Ord a => [[a]] -> [[a]]
+sortByHead = sortBy (\(x:_) (y:_) -> compare x y)
+
+-- Remove sublists off only -1
+removeSpoiled :: [[Int]] -> [[Int]]
+removeSpoiled = filter (not . all (==(-1)))
+
+-- Group elements on a matrix by their heads
+accom :: Eq a => [[a]] -> [[[a]]]
+accom = groupBy (\ (x:_) (y:_) -> x == y )
